ff4128cf557ec9d25f64b99059dfd430c98c4ad2
Ruben <26065182+AnotherRuben@users.noreply.github.com>
2025-05-19 13:52:50 +0200

    Revert "Updated website"
    This reverts commit d94b4ed2d2e46e428df91e9cf61c3edfc3d71625.

--- Index_files/855b35502a7aa4e8.en-GB.js
+++
@@ -1,2 +0,0 @@
-window['cmsg']['assets'] = window['cmsg']['assets'] || {};
-window['cmsg']['assets']["en-GB"] = Object.assign(window['cmsg']['assets']["en-GB"] || {}, {"15706":{"js":["cba61daa9af3b349.strings.js"],"css":[]},"38701":{"js":["8f7aaba4b2c782f9.strings.js"],"css":[]}});
